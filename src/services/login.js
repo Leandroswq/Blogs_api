6d13d05570dd163689c131a192a8f59633d8d7be
Leandroswq <leandrobonfim15@hotmail.com>
2022-07-14 10:38:55 -0300

    feat<login>: Criação do service verifyLogin
    O service verifica se os campos email e password estão nos formatos
    corretos. Caso não esteja lança um erro BadRequest com a mensagem
    "Some required fields are missing"

--- src/services/login.js
+++ src/services/login.js
@@ -1,4 +1,5 @@
 const httpErrors = require('http-errors');
+const Joi = require('joi');
 const jwt = require('jsonwebtoken');
 const { User } = require('../database/models/index');
 require('dotenv').config();
@@ -6,6 +7,17 @@ require('dotenv').config();
 const jwtSecrete = process.env.JWT_SECRET;
 
 module.exports = {
+  async verifyLogin(email, password) {
+    const { error } = Joi.object({
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    }).validate({ email, password });
+
+    if (error) {
+      throw new httpErrors.BadRequest('Some required fields are missing');
+    }
+  },
+
   async login(email, password) {
     const user = await User.findOne({
       where: { email },
