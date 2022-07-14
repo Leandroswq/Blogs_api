849cdafcc429b1077e49e3785913617ec5e4efaa
Leandroswq <leandrobonfim15@hotmail.com>
2022-07-14 17:54:58 -0300

    fix: Adiciona autoincrement ao id dos models
    Os models category e user estavam com o campo id sem o autIncrement.
    Isso estava fazendo com que ao criar um novo usuario ou categoria o
    sequelize retornava o id como null

--- src/database/models/category.js
+++ src/database/models/category.js
@@ -1,11 +1,24 @@
+/**
+ * @param {import('sequelize')} sequelize
+ * @param {import('sequelize').DataTypes} DataTypes
+ * @returns
+ */
 const category = (sequelize, DataTypes) => {
-  const category = sequelize.define('Category', {
-		id: {primaryKey: true, type: DataTypes.STRING},
-    name: DataTypes.STRING,
-  },{
-    underscored: true,
-    tableName: 'Categories'
-  });
+  const category = sequelize.define(
+    "Category",
+    {
+      id: {
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+				autoIncrement: true,
+      },
+      name: DataTypes.STRING,
+    },
+    {
+      tableName: "Categories",
+      timestamps: false,
+    }
+  );
 
   return category;
 };
--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,22 +1,29 @@
-/** 
- * @param {import('sequelize')} sequelize 
- * @param {import('sequelize').DataTypes} DataTypes 
- * @returns 
+/**
+ * @param {import('sequelize')} sequelize
+ * @param {import('sequelize').DataTypes} DataTypes
+ * @returns
  */
 const user = (sequelize, DataTypes) => {
-	const user = sequelize.define('User', {
-		id: {primaryKey: true, type: DataTypes.STRING},
-		displayName: DataTypes.STRING,
-		email: DataTypes.STRING,
-		password: DataTypes.STRING,
-		image: DataTypes.STRING,
-	},{
-		tableName: 'Users',
-		timestamps: false
-	});
-	
-	return user;
+  const user = sequelize.define(
+    "User",
+    {
+      id: {
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+				autoIncrement: true,
+      },
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      tableName: "Users",
+      timestamps: false,
+    }
+  );
+
+  return user;
 };
 
 module.exports = user;
-
