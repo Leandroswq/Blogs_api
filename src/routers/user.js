4fc3a56bd58bdb324eb4213bf246da4e79a4825b
Leandroswq <leandrobonfim15@hotmail.com>
2022-07-14 16:06:07 -0300

    feat<user>: Rota que lista todos os usuarios
    A rota também tem um middlewarer que valida se o token é valido

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const controllers = require('../controllers/user');
+const globalMiddlewares = require('../middlewares/global');
 
 const router = express.Router();
 
 router.post('/', controllers.createUser);
+router.get('/', globalMiddlewares.validateToken, controllers.getAll);
 
 module.exports = router;
