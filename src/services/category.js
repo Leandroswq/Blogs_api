606411c975daff41a13838e64dedec33f83f1ef4
Leandroswq <leandrobonfim15@hotmail.com>
2022-07-14 19:13:21 -0300

    feat<category>: Criação da validação do nome
    Verifica se o nome da categoria tem um formato valido

--- src/services/category.js
+++ src/services/category.js
@@ -1,6 +1,12 @@
+const Joi = require('joi');
+const httpErrors = require('http-errors');
 const { Category } = require('../database/models/index');
 
 module.exports = {
+  validateName(name) {
+    const { error } = Joi.string().required().validate(name);
+    if (error) throw new httpErrors.BadRequest('"name" is required');
+  },
   async createCategorie(name) {
     const category = await Category.create({ name });
 
