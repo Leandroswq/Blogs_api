ba8c45fc69464ec1b94658f3938c6fb3c0b7f385
Leandroswq <leandrobonfim15@hotmail.com>
2022-07-16 21:39:47 -0300

    feat<post>: Cria o controller que retorna um post pelo id
    O controller retorna o estado 200 junto com os dados do post com o id
    que estiver no parametro da requisição

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -8,16 +8,24 @@ module.exports = {
   async createPost(req, res) {
     const postData = req.body;
     const userId = req.user.id;
-  
+
     await service.validateCreatePost(postData);
     const post = await service.createPost(postData, userId);
 
     res.status(201).json(post);
   },
-  
+
   async getAll(_req, res) {
     const posts = await service.getAll();
 
     res.status(200).json(posts);
+  },
+ 
+  async getById(req, res) {
+    const { id } = req.params;
+
+    const post = await service.getById(id);
+    
+    res.status(200).json(post);
   },
 };
